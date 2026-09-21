da095a59c40d3e8d23f1435e8c4456ad27278801
Matthieu Coraleau <matthieu.coraleau@epitech.eu>
2026-09-21 18:21:38 +0200

    feat(rsa/): add a function for find the n module of two primes

---
+++ src/algorithms/rsa/n_mod.py
@@ -0,0 +1,13 @@
+##
+## EPITECH PROJECT, 2026
+## Mirro-my_PGP
+## File description:
+## n_mod
+##
+
+from typing import Optional, Tuple
+
+
+def n_mod(primes: Optional[Tuple[int, int]]) -> int:
+    p, q = primes
+    return p * q
