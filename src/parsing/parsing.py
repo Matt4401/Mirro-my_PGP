e0f7580a3e0dcc996b8d60b9480fdc7265079dd4
TomGatin <tom.gatin@epitech.eu>
2026-09-22 17:02:15 +0200

    refactor(parsing.py): remove unused isprime import and clean up mode logic

--- src/parsing/parsing.py
+++ src/parsing/parsing.py
@@ -10,7 +10,6 @@ from dataclasses import dataclass
 from typing import Optional, Sequence, Tuple
 from .valids_options import SYMMETRIC_SYSTEMS
 from .parser import build_parser
-from sympy import isprime
 
 
 @dataclass(frozen=True) # just to make it secure
@@ -30,7 +29,6 @@ def _mode_from_namespace(arguments):
     if arguments.cipher:
         return "c"
 
-    
     return "d"
 
 
@@ -42,10 +40,7 @@ def _validate_arguments(arguments, parser):
         parser.error("-g can only be used with the rsa crypto system")
     if mode == "g" and arguments.key is not None:
         parser.error("a key cannot be used with -g")
-    if mode == "g" :
-        for prime in arguments.primes:
-           if not isprime(prime):
-               parser.error(f"{prime} is not a prime number")
+
     if arguments.single_block and arguments.crypto_system not in SYMMETRIC_SYSTEMS:
         parser.error("-b can only be used with xor, aes, pgp-xor or pgp-aes")
     if mode != "g" and arguments.key is None:
