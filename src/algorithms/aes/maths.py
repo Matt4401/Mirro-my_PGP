bb90594a84e51afe441f75e4ec5d7d35f64442e9
TomGatin <tom.gatin@epitech.eu>
2026-09-22 16:48:48 +0200

    feat(aes/maths.py): refactor Galois Field multiplication and update MixColumns implementation accordingly

--- src/algorithms/aes/maths.py
+++ src/algorithms/aes/maths.py
@@ -1,9 +1,5 @@
-##
-## EPITECH PROJECT, 2026
-## my_pgp
-## File description:
-## maths
-##
+from functools import reduce
+from operator import xor
 
 def sub_bytes(state: list[list[int]], s_box: list[int]) -> list[list[int]]:
     """Apply the SubBytes transformation to the state using the S-box."""
@@ -25,11 +21,23 @@ def add_round_key(state: list[list[int]], round_key: list[list[int]]) -> list[li
     """Apply the AddRoundKey transformation to the state."""
     return [[s_byte ^ k_byte for s_byte, k_byte in zip(s_row, k_row)] for s_row, k_row in zip(state, round_key)]
 
+def gmul(a: int, b: int) -> int:
+    """Galois Field multiplication of two bytes."""
+    p = 0
+    for _ in range(8):
+        if b & 1:
+            p ^= a
+        hi_bit_set = a & 0x80
+        a <<= 1
+        if hi_bit_set:
+            a ^= 0x11b
+        b >>= 1
+    return p % 256
+
 def mix_columns(state: list[list[int]], mix_matrix: list[list[int]]) -> list[list[int]]:
     """Apply the MixColumns transformation to the state."""
-    return [[sum(mix_matrix[i][j] * state[j][k] for j in range(4)) % 256 for k in range(4)] for i in range(4)]
+    return [[reduce(xor, (gmul(mix_matrix[i][j], state[j][k]) for j in range(4))) for k in range(4)] for i in range(4)]
 
 def inv_mix_columns(state: list[list[int]], inv_mix_matrix: list[list[int]]) -> list[list[int]]:
     """Apply the InvMixColumns transformation to the state."""
-    return [[sum(inv_mix_matrix[i][j] * state[j][k] for j in range(4)) % 256 for k in range(4)] for i in range(4)]
-
+    return [[reduce(xor, (gmul(inv_mix_matrix[i][j], state[j][k]) for j in range(4))) for k in range(4)] for i in range(4)]
